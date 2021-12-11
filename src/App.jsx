a789ae78b18e7d49b15122a433369fb8c2005e90
kdavis010397 <kdavis010397@gmail.com>
2021-12-11 15:20:42 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const pusher = new Pusher("4bc9cb463c8e0cf1f124", { cluster: "us2" });
+const channel = pusher.subscribe("this-channel");
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pusher.js</h1>
+      <p>Testing...</p>
+      <button
+        onClick={() => {
+          channel.bind("some-event", (data) => {
+            console.log(hello);
+          });
+        }}
+      >
+        Pusher Event
+      </button>
+    </div>
+  );
+};
+
+export default App;
